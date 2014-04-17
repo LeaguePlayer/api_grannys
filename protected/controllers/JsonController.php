f84b5ac4acd7b1828aef46781912a08c9d87fbd4
leagueplayer <four.ibm-knight@hotmail.com>
2014-04-17 23:01:37 +0600

    fix3 api

--- protected/controllers/JsonController.php
+++ protected/controllers/JsonController.php
@@ -381,7 +381,8 @@ class JsonController extends Controller
 				$response['menu'][$n]['type']['title'] = SiteHelper::getCategoryBoardmenu($menu->id_type);
 				$response['menu'][$n]['title'] = $menu->title;
 				$response['menu'][$n]['price'] = $menu->price;
-				$response['image'][$n] = "{$domain}{$menu->getImageUrl('medium')}";
+				$response['image']['url'][$n] = "{$domain}{$menu->getImageUrl('medium')}";
+				$response['image']['title'][$n] = "{$menu->title}, {$menu->price} руб.";
 				if( count($menu->composition) > 0 )
 				{
 					$z = 0;
