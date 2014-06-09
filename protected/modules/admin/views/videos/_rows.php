285a8676eff69e258e15b4b6fbb9430f3186de4a
Lplayer <four.ibm-knight@hotmail.com>
2014-06-09 15:09:04 +0400

    f5

--- protected/modules/admin/views/videos/_rows.php
+++ protected/modules/admin/views/videos/_rows.php
@@ -2,6 +2,8 @@
 
 	<?php echo $form->textFieldControlGroup($model,'title',array('class'=>'span8','maxlength'=>255)); ?>
 
-	<?php echo $form->textAreaControlGroup($model,'video_string',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
+	<?php echo $form->textFieldControlGroup($model,'video_string',array('class'=>'span8','prepend' => 'http://www.youtube.com/watch?v=' )); ?>
+    
+    
 
 	<?php echo $form->dropDownListControlGroup($model, 'status', Videos::getStatusAliases(), array('class'=>'span8', 'displaySize'=>1)); ?>
